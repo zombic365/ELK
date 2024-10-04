9961bfb75b181b4e597536dff94543891d49036f
taebin <taebin@nhnenter.com>
2024-10-04 13:39:41 +0900

    update install.sh

--- elk-v815-install.sh
+++ elk-v815-install.sh
@@ -3,13 +3,18 @@
 
 ELK_PATH="/DATA/ELK"
 PKG_PATH="${ELK_PATH}/pkgs"
+SVC_NAME="elk"
 
 if [ -d ${ELK_PATH} ]; then
     if [ -d ${PKG_PATH} ]; then
-        cd ${PKG_PATH}
         for _pkg in elasticsearch logstash kibana; do
+            cd ${PKG_PATH}
+            if [ -f ${ELK_PATH}/${_pkg} ]; then
+                echo "Install -> ${ELK_PATH}/${_pkg} skip."
+                continue
+            fi
             curl -O https://artifacts.elastic.co/downloads/${_pkg}/${_pkg}-8.15.2-linux-x86_64.tar.gz
-            curl -O https://artifacts.elastic.co/downloads/${_pkg}/${_pkg}-8.15.2-linux-x86_64.tar.gz.sha512 |shasum -a 512 -c -
+            curl https://artifacts.elastic.co/downloads/${_pkg}/${_pkg}-8.15.2-linux-x86_64.tar.gz.sha512 |shasum -a 512 -c -
             if [ $? -eq 0 ]; then
                 tar -zxf ./${_pkg}-8.15.2-linux-x86_64.tar.gz -C ${ELK_PATH}/.
                 cd ${ELK_PATH}
@@ -18,3 +23,18 @@ if [ -d ${ELK_PATH} ]; then
         done
     fi
 fi
+
+if [ ! -f ${PKG_PATH}/${SVC_NAME}-instances.yml ]; then
+    cat <<EOF >${PKG_PATH}/${SVC_NAME}-instances.yml
+instances:
+- name: ${SVC_NAME}
+    dns: [ '${SVC_NAME}' ]
+    EOF
+fi
+
+${ELK_PATH}/elasticsearch/bin/elasticsearch-certutil \
+cert \
+--keep-ca-key \
+--pem \
+--in ${PKG_PATH}/${SVC_NAME}-instances.yml \
+--out ${PKG_PATH}/${SVC_NAME}-certs.zip
